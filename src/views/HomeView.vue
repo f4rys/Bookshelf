b11b785b08f52594bb3db40600fb07f313d8bf8c
f4rys <looxxon@gmail.com>
2024-04-29 17:00:44 +0200

    Update home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container mt-5">
     <div class="hero d-flex justify-content-center align-items-center">
-      <div class="text-center animate__animated animate__fadeInDown">
-        <h1 class="display-4 fw-bold mb-4">Bookshelf</h1>
+      <div class="text-center">
+        <h1 class="display-4 fw-bold mb-1 app-name">BOOKSHELF</h1>
         <p class="lead text-white">Explore a curated collection of public domain books!</p>
         <router-link class="btn btn-primary btn-lg" to="/books">Start Reading</router-link>
       </div>
@@ -21,22 +21,19 @@
       <h2 class="text-center mb-4">What Bookshelf Offers</h2>
       <div class="row">
         <div class="col-md-4">
-          <div class="card text-center h-100 py-4 animate__animated animate__fadeInUp">
-            <i class="fas fa-book fa-3x"></i>
+          <div class="card text-center h-100 py-5 my-2">
             <h3 class="card-title">Hand-Picked Collection</h3>
             <p class="card-text">Explore a curated selection of public domain books.</p>
           </div>
         </div>
         <div class="col-md-4">
-          <div class="card text-center h-100 py-4 animate__animated animate__zoomIn">
-            <i class="fas fa-search fa-3x"></i>
+          <div class="card text-center h-100 py-5 my-2">
             <h3 class="card-title">Libraries</h3>
             <p class="card-text">Find the nearest library.</p>
           </div>
         </div>
         <div class="col-md-4">
-          <div class="card text-center h-100 py-4 animate__animated animate__bounce">
-            <i class="fas fa-bookmark fa-3x"></i>
+          <div class="card text-center h-100 py-5 my-2">
             <h3 class="card-title">Progress Tracking</h3>
             <p class="card-text">Keep track of where you left off.</p>
           </div>
@@ -67,9 +64,7 @@
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 
-/* Animations from Animate.css */
-.animate__animated {
-  animation-duration: 1s;
-  animation-fill-mode: both;
+.app-name{
+  font-family: "Lucida Console", "Courier New", monospace;
 }
 </style>

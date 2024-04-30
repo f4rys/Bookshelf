e8337e670b180763111db04ddff48901c27c2ad9
f4rys <looxxon@gmail.com>
2024-04-30 22:44:49 +0200

    Fetch and update user's favourite books

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -4,7 +4,10 @@
       <img :src="book.coverUrl" alt="Book Cover" class="card-img img-fluid" style="width: 200px; height: auto;">
       <div class="card-body d-flex flex-column justify-content-between align-items-start">
         <div class="text-start">
-          <h4 class="card-title fs-1 mx-4 align-text-center">{{ book.title }}<button type="button" class="btn align-text-top"><img class="image-fluid" height="30" width="30" src=".././assets/star_outline.png"></button></h4> 
+          <h4 class="card-title fs-1 mx-4 align-text-center">{{ book.title }}<button type="button" class="btn align-text-top" @click="updateFavourite(book)">
+            <img v-if="book.isFavourite" class="image-fluid" height="30" width="30" src=".././assets/star.png">
+            <img v-if="!book.isFavourite" class="image-fluid" height="30" width="30" src=".././assets/star_outline.png">
+          </button></h4> 
           <p class="card-text fs-4 mx-4">{{ book.author }}</p> 
           <p class="card-text mx-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu</p>
         </div>
@@ -19,14 +22,37 @@
 <script>
 import { getDocs } from "firebase/firestore";
 import { booksRef } from '@/main.js';
+import { usersRef } from '@/main.js';
+import { getCurrentUser } from 'vuefire'
+import { getDoc, doc } from 'firebase/firestore';
+import { updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+
+var user = await getCurrentUser();
+const specificUserDocRef = doc(usersRef, user.uid);
+var userData;
+
+await getDoc(specificUserDocRef)
+    .then(docSnapshot => {
+      if (docSnapshot.exists) {
+        userData = docSnapshot.data();
+      }
+    });
+
+const userFavourites = userData["Favourite_books"];
 
 const querySnapshot = await getDocs(booksRef);
 const books = [];
 querySnapshot.forEach((doc) => {
+  var isFavourite;
+  if(userFavourites.includes(doc.id)){
+    isFavourite = true
+  }
   books.push({
+    "id": doc.id,
     "author": doc.data()["Author"], 
     "title": doc.data()["Title"],
-    "coverUrl": doc.data()["Cover_url"]
+    "coverUrl": doc.data()["Cover_url"],
+    "isFavourite": isFavourite
   });
 });
 
@@ -36,5 +62,26 @@ export default {
       books: books
     };
   },
+  methods:{
+    async updateFavourite(book){
+      if(!book.isFavourite){
+        await updateDoc(specificUserDocRef, {
+        Favourite_books: arrayUnion(book.id),
+        }).catch(error => {
+        console.log(error);
+        });
+        book.isFavourite = true
+      }
+      else{
+        await updateDoc(specificUserDocRef, {
+        Favourite_books: arrayRemove(book.id)
+        }).catch(error => {
+        console.log(error);
+        });
+        book.isFavourite = false
+      }
+
+    }
+  }
 };
 </script>

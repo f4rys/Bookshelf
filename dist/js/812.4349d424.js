7fad0c7c2415dd56ea90db064ebefa9d4c0051d9
f4rys <looxxon@gmail.com>
2024-04-30 23:18:20 +0200

    New dist

--- dist/js/812.4349d424.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkbookshelf"]=self["webpackChunkbookshelf"]||[]).push([[812],{9812:function(n,u,e){e.r(u),e.d(u,{default:function(){return k}});var o=e(6768);const r=(0,o.Lk)("h1",null,"Books",-1),s=[r];function t(n,u){return(0,o.uX)(),(0,o.CE)("div",null,s)}var l=e(1241);const c={},f=(0,l.A)(c,[["render",t]]);var k=f}}]);
-//# sourceMappingURL=812.4349d424.js.map

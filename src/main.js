318c5b8f061840d8b1a0bdc6745472ccb06674e4
f4rys <looxxon@gmail.com>
2024-07-12 20:19:47 +0200

    Update firebase project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { initializeApp } from 'firebase/app'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { getFirestore, collection } from 'firebase/firestore'
@@ -11,12 +10,13 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export const firebaseApp = initializeApp({
-    apiKey: "AIzaSyDEPKlqj6wIO8Vz4OHIrdCkaFYEbopqDKc",
-    authDomain: "bookapp-302e2.firebaseapp.com",
-    projectId: "bookapp-302e2",
-    storageBucket: "bookapp-302e2.appspot.com",
-    messagingSenderId: "254959568814",
-    appId: "1:254959568814:web:54863ac7805fdbd7576a3f"
+    apiKey: "AIzaSyCT_KOnb23EtDhDKWmu-K2nt3B_RxccNLo",
+    authDomain: "bookshelf-2c44f.firebaseapp.com",
+    projectId: "bookshelf-2c44f",
+    storageBucket: "bookshelf-2c44f.appspot.com",
+    messagingSenderId: "916356885521",
+    appId: "1:916356885521:web:cda0cfffafe67407a9d5b0",
+    measurementId: "G-7WB07KJS2L"
 })
 
 const db = getFirestore(firebaseApp)
